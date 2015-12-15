b496f48fcc33d925b7ee2bfe6a0d0d012e7dff22
Mikael Sahlström <mick.sahlstrom@gmail.com>
2015-12-15 11:37:26 +0100

    Diode can now transfer packets.

--- src/app/nic_diode/main.cc
+++ src/app/nic_diode/main.cc
@@ -14,6 +14,7 @@ struct Server::Main
 {
   Allocator&alloc;
   Entrypoint&ep;
+
   enum { BUF_SIZE = Nic::Packet_allocator::DEFAULT_PACKET_SIZE * 128 };
 
   Allocator_avl inner_alloc{&alloc};
@@ -24,24 +25,80 @@ struct Server::Main
 
   Main(Allocator&alloc, Entrypoint &ep) : alloc(alloc), ep(ep)
   {
+    // There is room in the send queue.
     nic_in.tx_channel()->sigh_ready_to_submit (packet_dispatcher);
+    // There is a ack in the ack queue.
     nic_in.tx_channel()->sigh_ack_avail       (packet_dispatcher);
+
+    // There is room in the ack queue.
     nic_in.rx_channel()->sigh_ready_to_ack    (packet_dispatcher);
+    // There is a packet in the receive queue.
     nic_in.rx_channel()->sigh_packet_avail    (packet_dispatcher);
 
+    // There is room in the send queue.
     nic_out.tx_channel()->sigh_ready_to_submit (packet_dispatcher);
+    // There is a ack in the ack queue.
     nic_out.tx_channel()->sigh_ack_avail       (packet_dispatcher);
+
+    // There is room in the ack queue.
     nic_out.rx_channel()->sigh_ready_to_ack    (packet_dispatcher);
+    // There is a packet in the receive queue.
     nic_out.rx_channel()->sigh_packet_avail    (packet_dispatcher);
   };
 
   void handle_packet(unsigned)
   {
     PDBG("Got packet\n");
+
+    for (;;)
+    {
+      while (nic_in.tx()->ack_avail())
+      {
+        PDBG("inner release packet\n");
+        nic_in.tx()->release_packet(nic_in.tx()->get_acked_packet());
+      }
+
+      if (!nic_in.tx()->ready_to_submit())
+      {
+        return;
+      }
+      PDBG("inner ready to submit\n");
+
+      if (!nic_out.rx()->ready_to_ack())
+      {
+        return;
+      }
+      PDBG("outer ready to ack\n");
+
+      if (!nic_out.rx()->packet_avail())
+      {
+        return;
+      }
+      PDBG("outer packet avail\n");
+
+      //size_t const packet_size = nic_out.rx()->peek_packet().size();
+      Packet_descriptor packet_for_inner;
+      try
+      {
+        packet_for_inner = nic_in.tx()->alloc_packet(2048);
+      }
+      catch (...)
+      {
+        return;
+      }
+      PDBG("inner tx packet alloced\n");
+
+      Packet_descriptor const packet_from_outer = nic_out.rx()->get_packet();
+      Genode::memcpy(nic_in.tx()->packet_content(packet_for_inner),
+                     nic_out.rx()->packet_content(packet_from_outer),
+                     2048);
+      nic_in.tx()->submit_packet(packet_for_inner);
+      nic_out.rx()->acknowledge_packet(packet_from_outer);
+      PDBG("Packet sent\n");
+    }
   }
 
-  Signal_rpc_member<Main> packet_dispatcher =
-        { ep, *this, &Main::handle_packet};
+  Signal_rpc_member<Main> packet_dispatcher = { ep, *this, &Main::handle_packet };
 };
 
 namespace Server
