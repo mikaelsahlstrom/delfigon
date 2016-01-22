a2c731c5f357a48f97a3c73ebd71d3322be7f26a
Pontus Åström <pontus.astrom@delfigon.com>
2016-01-22 12:27:02 +0100

    First version with compiling but not correctly executing nic_diode_sm

--- include/state_machine/hsm.h
+++ include/state_machine/hsm.h
@@ -13,14 +13,23 @@
          * Implement Queued HSM and Queued HSM with threads for more advanced
 		   state machines.
  */
+/*
 #include <assert.h>
+#include <deque>
+*/
 #include "state.h"
 
 namespace hsm
 { 
+	template<typename H, typename E> class Basic;
+	template<typename H, typename E> class Queued;
+}	
 
+/******************************************************************************
+ ** Basic declaration
+ ******************************************************************************/
 template<typename H, typename E>
-class Basic
+class hsm::Basic
 {
 public:
 	typedef E Event;
@@ -29,30 +38,138 @@ protected:
 	typedef H Hsm;
 	typedef state::Top<Hsm> State;
 	
-	const State * state_;
-	Event event_;
+	const State * _state;
+	Event         _event;
+
+public:
+	Basic();
+	~Basic();
+
+	void         next(const State &);
+	Event        get_event() const;
+	virtual void dispatch(Event);
+};
+
+/******************************************************************************
+ ** Queued declaration
+ ******************************************************************************/
+
+/*
+template<typename H, typename E>
+class hsm::Queued : hsm::Basic<H, E>
+{
+public:
+	typedef H Hsm;
+	typedef E Event;
+	typedef hsm::Basic<H, E> Base;
+	typedef unsigned char Priority;
+	static const Priority default_priority = static_cast<Priority>(1);
+	
+private:
+	typedef std::deque<Event> Event_queue;
+
+	Priority    _priority;
+	Event_queue _event_queue;
 	
 public:
-	Basic()
-		: event_(Event()),
-		  state_(nullptr)
-		{};
-	~Basic() {}
-	
-	void next(const State & state)
-	{
-		state_ = & state;
-	}
-	Event get_event() const
-	{
-		return event_;
-	}
-	void dispatch(Event e)
-	{
-		assert(state_ != nullptr);
-		event_ = e;
-		state_->do_handler(*static_cast<Hsm*>(this));
-		state_->event_handler(*static_cast<Hsm*>(this));
-	}
+	Queued(Priority arg = default_priority);
+	~Queued() {}
+
+	void     dispatch();
+	void     post(Event);
+	void     post_front(Event);
+	Priority get_priority() const;
+	void     set_priority(Priority);
 };
-} /* namespace hsm */
+
+*/
+/******************************************************************************
+ ** Basic definition
+ ******************************************************************************/
+template<typename H, typename E>
+hsm::Basic<H, E>::Basic()
+	: _state(nullptr),
+	  _event(Event())
+{
+}
+
+template<typename H, typename E>
+hsm::Basic<H, E>::~Basic() {}
+	
+template<typename H, typename E>
+void
+hsm::Basic<H, E>::next(const State & arg)
+{
+	_state = & arg;
+}
+
+template<typename H, typename E>
+typename hsm::Basic<H, E>::Event
+hsm::Basic<H, E>::get_event() const
+{
+	return _event;
+}
+
+template<typename H, typename E>
+void
+hsm::Basic<H, E>::dispatch(Event arg)
+{
+	/*	assert(_state != nullptr); */
+	_event = arg;
+	_state->do_handler(*static_cast<Hsm*>(this));
+	_state->event_handler(*static_cast<Hsm*>(this));
+}
+
+
+/******************************************************************************
+ ** Queued definition
+ ******************************************************************************/
+
+/*
+
+template<typename H, typename E>
+hsm::Queued<H, E>::Queued(Priority arg)
+	: Base(),
+	  _priority(arg)
+{
+}
+
+template<typename H, typename E>
+void
+hsm::Queued<H, E>::dispatch()
+{
+	if (! _event_queue.empty()) {
+		dispatch(_event_queue.front());
+		_event_queue.pop_front();
+	}
+}
+
+template<typename H, typename E>
+void
+hsm::Queued<H, E>::post(Event arg)
+{
+	_event_queue.push_back(arg);
+}
+
+template<typename H, typename E>
+void
+hsm::Queued<H, E>::post_front(Event arg)
+{
+	_event_queue.push_front(arg);
+}
+
+template<typename H, typename E>
+typename hsm::Queued<H, E>::Priority
+hsm::Queued<H, E>::get_priority() const
+{
+	return _priority;
+}
+
+template<typename H, typename E>
+void
+hsm::Queued<H, E>::set_priority(Priority arg)
+{
+	_priority = arg;
+}
+
+*/
--- include/state_machine/state.h
+++ include/state_machine/state.h
@@ -10,187 +10,285 @@
  * as target for a transaction.
  */
 
-namespace hsm {
-namespace state {
+namespace hsm { namespace state {
+	template<typename H> struct Top;
+   template<typename H, unsigned ID, typename P> struct Composite;
+	template<typename H, unsigned ID, typename P> struct Leaf;
+	template<bool>                                class  Bool {};
+	template<typename C, typename P>              class  Is_child;
+	template<typename C>                          struct Init;
+	template<typename C, typename S, typename T>	 struct Transition;
+	}
+}
 
+/******************************************************************************
+ ** Top declaration
+ ******************************************************************************/
 template<typename H>
-struct Top
+struct hsm::state::Top
 {
 	typedef H Hsm;
 	typedef void Parent;
 
-	virtual void event_handler(Hsm &) const = 0;
-	virtual void do_handler(Hsm &) const = 0;
+	virtual void     event_handler(Hsm &) const = 0;
+	virtual void     do_handler(Hsm &) const = 0;
 	virtual unsigned get_id() const = 0;
 };
 
-template<typename H, unsigned ID, typename P> struct Composite;
 
-template<typename H, unsigned ID, typename P = Composite<H, 0, Top<H>>>
-struct Composite : P
+/******************************************************************************
+ ** Composite declaration
+ ******************************************************************************/
+template<typename H, unsigned ID,
+			typename P = hsm::state::Composite<H, 0, hsm::state::Top<H>>>
+struct hsm::state::Composite : P
 {
 	typedef H Hsm;
 	typedef P Parent;
 	typedef Composite<Hsm, ID, Parent> This;
 
-	virtual void do_handler(Hsm & h) const
-		{
-			Parent::do_handler(h);
-			handle_do(h, * this);
-		};
+	virtual void do_handler(Hsm &) const;
 
-	template<typename LEAF>	void handle_do(Hsm & h, const LEAF & l) const {}
-	template<typename LEAF>	void handle_event(Hsm & h, const LEAF & l) const
+	template<typename LEAF>	void handle_do   (Hsm &, const LEAF &) const {}
+	template<typename LEAF>	void handle_event(Hsm &, const LEAF &) const;
+
+	/* Provide no default implementation as 1) no default
+	 * implementation makes sense and 2) to achieve compile time
+	 * failure in case specialization is forgotten. */
+	static void handle_init (Hsm &);
+	static void handle_entry(Hsm &) {}
+	static void handle_exit (Hsm &) {}
+};
+
+
+/******************************************************************************
+ ** Composite specialization for the Top state
+ ******************************************************************************/
+namespace hsm { namespace state {
+	template<typename H>
+	struct Composite<H, 0, Top<H>> : Top<H>
 	{
-		Parent::handle_event(h, l);
-	}
-	static void handle_entry(Hsm &) {}
-	static void handle_init(Hsm &);
-	static void handle_exit(Hsm &) {}
-};
+		typedef H Hsm;
+		typedef Top<Hsm> Parent;
+		typedef Composite<Hsm, 0, Parent> This;
+		
+		virtual void do_handler(Hsm &) const {};
+		
+		template<typename THIS>	void handle_do   (Hsm &, const THIS &) const {}
+		template<typename THIS>	void handle_event(Hsm &, const THIS &) const {}
+		
+		static void handle_init (Hsm &);
+		static void handle_entry(Hsm &) {}
+		static void handle_exit (Hsm &) {}
+	};
+}
+}
 
-// Specialization for the top state
-template<typename H>
-struct Composite<H, 0, Top<H>> : Top<H>
-{
-	typedef H Hsm;
-	typedef Top<Hsm> Parent;
-	typedef Composite<Hsm, 0, Parent> This;
-
-	virtual void do_handler(Hsm &) const {};
-	template<typename THIS>	void handle_do   (Hsm &, const THIS &) const {}
-	template<typename THIS>	void handle_event(Hsm &, const THIS &) const {}
-	static void handle_entry(Hsm &) {}
-	static void handle_init(Hsm &); // no implementation
-	static void handle_exit(Hsm &) {}
-};
-
-template<typename H, unsigned ID, typename P = Composite<H, 0, Top<H>>>
-struct Leaf : P
+/******************************************************************************
+ ** Leaf declaration
+ ******************************************************************************/
+template<typename H, unsigned ID,
+			typename P = hsm::state::Composite<H, 0, hsm::state::Top<H>>>
+struct hsm::state::Leaf : P
 {
 	typedef H Hsm;
 	typedef P Parent;
 	typedef Leaf<Hsm, ID, Parent> This;
 	
-	virtual void do_handler(Hsm & h) const override
-	{
-		Parent::do_handler(h);
-		handle_do(h, * this);
-	}
-	template<typename THIS>
-	void handle_do(Hsm & h, const THIS & t) const
-	{
-	}
-	template<typename THIS>
-	void handle_event(Hsm & h, const THIS & t) const
-	{
-		Parent::handle_event(h, t);
-	}
-	virtual void event_handler(Hsm & h) const override 
-	{
-		handle_event(h, * this);
-	}
-	virtual unsigned get_id() const override
-	{
-		return ID;
-	}
-	static void handle_init(Hsm & h)
-	{
-		h.next(obj);
-	}
-	// don't specialize this
-	static void handle_entry(Hsm &) {}
-	static void handle_exit(Hsm &) {}
+	virtual void     do_handler   (Hsm &) const override;
+	virtual void     event_handler(Hsm &) const override;
+	virtual unsigned get_id()             const override;
+
+	template<typename THIS>	void handle_do   (Hsm &, const THIS &) const {}
+	template<typename THIS>	void handle_event(Hsm &, const THIS &) const;
+
+	static void handle_init (Hsm &);
+	static void handle_entry(Hsm &) {} 	
+	static void handle_exit (Hsm &) {}
+
 	static const Leaf obj;
 };
 
-template<typename H, unsigned ID, typename P> 
-const Leaf<H, ID, P> Leaf<H, ID, P>::obj;
 
-template<bool> class Bool {};
-
-template<typename C, typename P>
-class Is_child
-{
-	typedef C Child;
-	typedef P Parent;
-	
-	class Yes {	char a[1]; };
-	class No  {	char a[5]; };
-	static Yes test( Parent * ); // undefined
-	static No test( ... );    // undefined
-public:
-	enum { Res = sizeof(test(static_cast<Child *>(0))) == sizeof(Yes) ? 1 : 0 };
-};
-
+/******************************************************************************
+ ** Init declaration
+ ******************************************************************************/
 template<typename C>
-struct Init
+struct hsm::state::Init
 {
 	typedef C Child;
 	typedef typename C::Hsm Hsm;
 
-	Init(Hsm & h)
-		: hsm_(h) {}
-	~Init()
-	{
-		Child::handle_entry(hsm_);
-		Child::handle_init(hsm_);
-	}
-	Hsm & hsm_;
+	Init(Hsm &);
+	~Init();
+	
+	Hsm & _hsm;
 };
 
+/******************************************************************************
+ ** Is_child definition
+ ** See GOTW #71 for design
+ ******************************************************************************/
+template<typename C, typename P>
+class hsm::state::Is_child
+{
+	typedef C   Child;
+	typedef P   Parent;
+	class   Yes { char a[1]; };
+	class   No  { char a[5]; };
+	static  Yes test( Parent * ); // undefined
+	static  No  test( ... );    // undefined
+
+public:
+	enum { Res = sizeof(test(static_cast<Child *>(0))) == sizeof(Yes) ? 1 : 0 };
+};
+
+/******************************************************************************
+ ** Transition declaration
+ ******************************************************************************/
 template<typename C, typename S, typename T>
-struct Transition
+struct hsm::state::Transition
 {
 	typedef C Current;
 	typedef S Source;
 	typedef T Target;
-	typedef typename Current::Hsm Hsm;
+	typedef typename Current::Hsm    Hsm;
 	typedef typename Current::Parent Current_parent;
-	typedef typename Target::Parent Target_parent;
+	typedef typename Target::Parent  Target_parent;
 	enum { // work out when to terminate template recursion
-		eTB_CB = Is_child<Target_parent, Current_parent>::Res,
-		eS_CB  = Is_child<S, Current_parent>::Res,
-		eS_C   = Is_child<S, C>::Res,
-		eC_S   = Is_child<C, S>::Res,
-		exitStop = eTB_CB && eS_C,
-		entryStop = eS_C || eS_CB && !eC_S
+		eTB_CB    = Is_child<Target_parent, Current_parent>::Res,
+		eS_CB     = Is_child<S, Current_parent>::Res,
+		eS_C      = Is_child<S, C>::Res,
+		eC_S      = Is_child<C, S>::Res,
+		exitStop  = eTB_CB && eS_C,
+		entryStop = eS_C || (eS_CB && !eC_S)
 	};
-	Transition(Hsm & h)
-		: hsm_(h)
-	{
-		exit_actions(hsm_, Bool<false>());
-	}
-	~Transition()
-	{
-		typedef Transition<Target, Source, Target> Trans;
 
-		Trans::entry_actions(hsm_, Bool<false>());
-		T::handle_init(hsm_);
-	}
+	Transition(Hsm &);
+	~Transition();
 	// We use overloading to stop recursion. The more natural template
 	// specialization method would require to specialize the inner
 	// template without specializing the outer one, which is
 	// forbidden.
-	static void exit_actions(Hsm &, Bool<true>) {}
-	static void exit_actions(Hsm & h, Bool<false>)
-	{
-		typedef Transition<Current_parent, Source, Target> Trans; 
-
-		C::handle_exit(h);
-		Trans::exit_actions(h, Bool<exitStop>());
-	}
-	static void entry_actions(Hsm &, Bool<true>) {}
-	static void entry_actions(Hsm & h, Bool<false>)
-	{
-		typedef Transition<Current_parent, Source, Target> Trans; 
-
-		Trans::entry_actions(h, Bool<entryStop>());
-		C::handle_entry(h);
-	}
+	static void exit_actions (Hsm &  , Bool<true>) {}
+	static void exit_actions (Hsm & h, Bool<false>);
+	static void entry_actions(Hsm &  , Bool<true>) {}
+	static void entry_actions(Hsm & h, Bool<false>);
 
 private:
-	Hsm & hsm_;
+	Hsm & _hsm;
 };
-} /* closes namespace state */
-} /* closes namespace hsm */
+
+/******************************************************************************
+ ** Composite definition
+ ******************************************************************************/
+template<typename H, unsigned ID, typename P>
+void
+hsm::state::Composite<H, ID, P>::do_handler(Hsm & arg) const
+{
+	Parent::do_handler(arg);
+	handle_do(arg, * this);
+};
+
+template<typename H, unsigned ID, typename P>
+template<typename LEAF>
+void
+hsm::state::Composite<H, ID, P>::handle_event(Hsm & h, const LEAF & l) const
+{
+	Parent::handle_event(h, l);
+}
+
+/******************************************************************************
+ ** Leaf definition
+ ******************************************************************************/
+template<typename H, unsigned ID, typename P>
+void
+hsm::state::Leaf<H, ID, P>::do_handler(Hsm & arg) const
+{
+	Parent::do_handler(arg);
+	handle_do(arg, * this);
+}
+
+template<typename H, unsigned ID, typename P>
+template<typename THIS>
+void
+hsm::state::Leaf<H, ID, P>::handle_event(Hsm & h, const THIS & t) const
+{
+	Parent::handle_event(h, t);
+}
+
+template<typename H, unsigned ID, typename P>
+void
+hsm::state::Leaf<H, ID, P>::event_handler(Hsm & arg) const
+{
+	handle_event(arg, * this);
+}
+
+template<typename H, unsigned ID, typename P>
+unsigned
+hsm::state::Leaf<H, ID, P>::get_id() const
+{
+	return ID;
+}
+
+template<typename H, unsigned ID, typename P>
+void
+hsm::state::Leaf<H, ID, P>::handle_init(Hsm & arg)
+{
+	arg.next(obj);
+}
+
+template<typename H, unsigned ID, typename P> 
+const hsm::state::Leaf<H, ID, P> hsm::state::Leaf<H, ID, P>::obj {};
+
+/******************************************************************************
+ * Init definition
+ ******************************************************************************/
+template<typename C>
+hsm::state::Init<C>::Init(Hsm & arg)
+	: _hsm(arg)
+{
+}
+
+template<typename C>
+hsm::state::Init<C>::~Init()
+{
+	Child::handle_entry(_hsm);
+	Child::handle_init (_hsm);
+}
+
+/******************************************************************************
+ ** Transition definition
+ ******************************************************************************/
+template<typename C, typename S, typename T>
+hsm::state::Transition<C, S, T>::Transition(Hsm & arg)
+	: _hsm(arg)
+{
+	exit_actions(_hsm, Bool<false>());
+}
+
+template<typename C, typename S, typename T>
+hsm::state::Transition<C, S, T>::~Transition()
+{
+	typedef Transition<Target, Source, Target> Trans;
+	Trans::entry_actions(_hsm, Bool<false>());
+	T::handle_init(_hsm);
+}
+
+template<typename C, typename S, typename T>
+void
+hsm::state::Transition<C, S, T>::exit_actions(Hsm & h, Bool<false>)
+{
+	typedef Transition<Current_parent, Source, Target> Trans; 
+	C::handle_exit(h);
+	Trans::exit_actions(h, Bool<exitStop>());
+}
+
+template<typename C, typename S, typename T>
+void
+hsm::state::Transition<C, S, T>::entry_actions(Hsm & h, Bool<false>)
+{
+	typedef Transition<Current_parent, Source, Target> Trans; 
+	Trans::entry_actions(h, Bool<entryStop>());
+	C::handle_entry(h);
+}
